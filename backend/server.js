b4e77a41a385244045d3eef6be2369a7fd7b19c6
Aravind163 <Aravindsiddharthp@gmail.com>
2026-08-06 15:50:53 +0530

    updation

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json());
+app.use(cors({ origin: "https://employee-enrollment-system-kappa.vercel.app", credentials: true }));app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
